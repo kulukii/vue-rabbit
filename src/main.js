48cbda46512d25d1e6a66b444d9bfd976acced6e
cc <2695231332@qq.com>
2025-05-17 11:17:34 +0800

    吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -9,11 +9,6 @@ import router from './router'
 // 引入初始化样式
 import '@/styles/common.scss'
 
-// test
-import { getCategory } from './apis/testAPI'
-getCategory().then(res => {
-  console.log(res);
-})
 
 const app = createApp(App)
 
