20f731e621d86b0c297de464508b6e2bc56c58e5
cc <2695231332@qq.com>
2025-05-17 21:15:08 +0800

    home-面板组件搭建

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,11 @@ import HomeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+  <!-- panel测试 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+
+  </HomePanel>
+  <HomePanel title="人气推荐">
+
+  </HomePanel>
 </template>
