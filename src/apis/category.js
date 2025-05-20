5423c4adfed294d0a38be0da12944abef0b4d546
cc <2695231332@qq.com>
2025-05-20 10:34:03 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -9,3 +9,19 @@ export function getCategoryAPI(id) {
     }
   })
 }
+
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 二级分类id
+ * @return {*}
+ */
+
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url:'/category/sub/filter',
+    params:{
+      id
+    }
+  })
+}
