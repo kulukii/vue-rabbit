e262948f4eff1929493ffe369be211b6f9572cae
cc <2695231332@qq.com>
2025-05-20 21:19:47 +0800

    路由行为定制

--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -43,6 +43,19 @@ const tabChange = () => {
   reqData.value.page = 1
   getGoodList()
 }
+
+// 加载更多
+const disabled=ref(false)
+const load = async () => {
+  // console.log('加载更多数据咯');
+  // 获取下一页
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -57,12 +70,12 @@ const tabChange = () => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange" >
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods=goods :key="goods.id" />
       </div>
