5ccde925215cd626090e7d093e10ea730a35d199
cc <2695231332@qq.com>
2025-05-26 17:23:30 +0800

    登录-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,20 @@
 <script setup>
+import { ref } from 'vue';
 
+const form = ref({
+  account: '',
+  password: ''
+})
+
+const rules = {
+  account: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+     { min: 6, max: 24, message: '密码长度要求6-14个字符' }
+  ]
+}
 </script>
 
 
@@ -24,16 +39,15 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
-              status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+            <el-form label-position="right" label-width="60px" status-icon :model="form" :rules="rules">
+              <el-form-item label="账户" prop="account">
+                <el-input v-model="form.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="form.password"/>
               </el-form-item>
               <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+                <el-checkbox size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
