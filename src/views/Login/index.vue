610c06dfa2190a2d52fa087c988a2952cb452fe1
cc <2695231332@qq.com>
2025-05-26 20:03:41 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -27,6 +27,18 @@ const rules = {
     }
   ]
 }
+
+// 获取表单实例
+const formRef = ref(null)
+
+const doLogin = () => {
+  formRef.value.validate((valid) => {
+    // console.log(valid);
+    if(valid){
+      // TODO
+    }
+  })
+}
 </script>
 
 
@@ -51,7 +63,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon :model="form" :rules="rules">
+            <el-form label-position="right" label-width="60px" status-icon :model="form" :rules="rules" ref="formRef">
               <el-form-item label="账户" prop="account">
                 <el-input v-model="form.account" />
               </el-form-item>
@@ -63,7 +75,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
