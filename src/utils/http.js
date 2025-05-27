799f66e2a41a12d38b24c2c34a37df369739df73
cc <2695231332@qq.com>
2025-05-27 16:08:31 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,12 +2,15 @@ import axios from 'axios'
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 import { userdefineStore } from '@/stores/user'
+import router from '@/router';
+
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
 })
 
+// axios请求拦截器
 httpInstance.interceptors.request.use(config => {
   const userStore = userdefineStore()
   const token = userStore.userInfo.token
@@ -24,6 +27,13 @@ httpInstance.interceptors.response.use(res => res.data, e => {
     type: 'warning',
     message: e.response.data.message
   })
+  // 401
+  if (e.response.status === 401) {
+    const userStore = userdefineStore()
+    userStore.clearUserInfo()
+    router.push('/login')
+
+  }
   return Promise.reject(e)
 })
 
