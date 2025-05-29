0f1f5a83a0afb21ad04eea8dc85afbdf1a072720
cc <2695231332@qq.com>
2025-05-29 16:35:36 +0800

    本地购物车-列表购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -19,6 +19,13 @@ export const useCartStore = defineStore('cart', () => {
     cartList.value.splice(idx, 1)
   }
 
+
+  // 单选功能
+  const singleCheck = (skuId, selected) => {
+    const item = cartList.value.find((item) => item.skuId === skuId)
+    item.selected=selected
+  }
+
   // 计算属性
   // 总数
   const allCount = computed(() => cartList.value.reduce((pre, cur) => { return pre + cur.count }, 0))
@@ -29,7 +36,8 @@ export const useCartStore = defineStore('cart', () => {
     addCart,
     delCart,
     allCount,
-    allPrice
+    allPrice,
+    singleCheck
   }
 }, {
   persist: true
