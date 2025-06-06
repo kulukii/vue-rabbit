cff5de641fea77b8a73d881267c6124f44f1a1a8
cc <2695231332@qq.com>
2025-06-06 20:09:03 +0800

    会员中心-个人信息渲染

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import cartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/CheckOut/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -43,9 +46,22 @@ const router = createRouter({
         {
           path: 'pay',
           component: Pay
-        },{
-          path:'paycallback',
-          component:PayBack
+        }, {
+          path: 'paycallback',
+          component: PayBack
+        }, {
+          path: 'member',
+          component: Member,
+          children:[
+            {
+              path:'user',
+              component:UserInfo
+            },
+            {
+              path:'order',
+              component:UserOrder
+            }
+          ]
         }
       ]
     },
